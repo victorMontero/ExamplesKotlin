9460a377f756b55e1bb4770772dff84113ba7fbe
unknown <viiktoor@gmail.com>
2020-07-08 08:09:05 -0400

    novas tentativas

--- src/arrays/TestaCaracterPalavra.kt
+++ src/arrays/TestaCaracterPalavra.kt
@@ -6,18 +6,21 @@ fun main() {
 
 }
 
+// tentar outra abordagem, recebendo string mesmo, comparando (mas aí o melhor jeito é ter indice de array, pra poder comparar mesmo.
+
 fun testaPalavra(palavra: String) {
     val listaPalavra: Array<String> = palavra.toCharArray().map { it.toString() }.toTypedArray()
-    val verificarLetra = listaPalavra[0]
 
     listaPalavra.forEachIndexed { i, letra ->
+        var verificarLetra = listaPalavra[i]
+
         if(listaPalavra.contains(verificarLetra)){
             println(true)
         } else {
             println(false)
-
         }
     }
+
 
 //    for (letra in listaPalavra) {
 //        if (listaPalavra.contains(letra)) {
